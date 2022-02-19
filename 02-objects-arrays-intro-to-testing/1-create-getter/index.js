707f8a66c737f27a89da1f8b83e8cdcecd8e9edb
Volodymyr Shevchuk <dosandkv@gmail.com>
2022-02-20 01:12:51 +0200

    Added solution for module #2

--- 02-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 02-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,29 +4,5 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-  const arr = path.split('.');
 
-  return obj => {
-    if (Object.keys(obj).length === 0) return;
-
-    const GetVal = (index, newObj) => {
-      const result = [undefined];
-
-      if ((index === arr.length - 1) && (typeof newObj === 'object') && (typeof obj !== undefined)) {
-        console.error(1);
-        if (newObj.hasOwnProperty(arr[index])) {
-          result.unshift(newObj[arr[index]]);
-        }
-      }
-      else if ((typeof obj === 'object') && (typeof obj !== undefined)) {
-        console.error(2);
-
-        result.unshift(GetVal(index+1,newObj[arr[index]]))
-      }
-
-      return result[0];
-    };
-
-    return GetVal(0,obj);
-  };
 }
